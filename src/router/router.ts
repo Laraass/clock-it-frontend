56b033e55a2bbe4a1b9d2c9258273a127773d98a
Lara <lara.salman01@hotmail.com>
2025-04-20 19:33:51 +0200

    add router

---
+++ src/router/router.ts
@@ -0,0 +1,31 @@
+import { Home } from '../views/Home';
+import { Register } from '../views/Register';
+import { SignIn } from '../views/SignIn';
+import { TimeReports } from '../views/TimeReports';
+import { AllTimeReports } from '../views/AllTimeReports';
+import { CreateTimeReport } from '../views/CreateTimeReport';
+import { EditTimeReport } from '../views/EditTimeReport';
+import { SpecificTimeReport } from '../views/SpecificTimeReport';
+
+export function getViewByRoute(route: string): string {
+  switch (route) {
+    case '/':
+      return Home();
+    case '/register':
+      return Register();
+    case '/signin':
+      return SignIn();
+    case '/timereports':
+      return TimeReports();
+    case '/timereports/all':
+      return AllTimeReports();
+    case '/timereports/create':
+      return CreateTimeReport();
+    case '/timereports/edit':
+      return EditTimeReport();
+    case '/timereports/specific':
+      return SpecificTimeReport();
+    default:
+      return `<h1 class="text-xl text-shade-800 p-4">404 Page does not exist </h1>`;
+  }
+}
