6484befb1f5f974c1b9ffc159018e3f797f46b91
Lara <lara.salman01@hotmail.com>
2025-04-20 02:24:23 +0200

    create task component

---
+++ src/components/Task.ts
@@ -0,0 +1,15 @@
+export function Task(task: string): string {
+  return `
+      <div
+        class="flex items-center justify-center text-center bg-primary text-shade-800 px-4 py-2 rounded-md border-2 border-secondary
+               size-32 
+               font-medium
+               cursor-pointer
+               hover:shadow-[inset_0_0_20px_5px_#5FA3D7] 
+               active:shadow-[inset_0_0_20px_5px_#5FA3D7] 
+               transition-all duration-300"
+      >
+        ${task}
+      </div>
+    `;
+}
