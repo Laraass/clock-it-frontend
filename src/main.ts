d6242d30d9099dda7823ad1412ed4f08ea193bb9
Lara <lara.salman01@hotmail.com>
2025-04-21 00:57:42 +0200

    add children to layout component

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
-import './style.css';
-import { Layout } from './components/Layout';
-import { setupNavbarListeners } from './components/Navbar';
-import { handleBackClick } from './utils/backLogic';
+import "./style.css";
+import { Layout } from "./components/Layout";
+import { setupNavbarListeners } from "./components/Navbar";
+import { handleBackClick } from "./utils/backLogic";
+import { SignIn } from "./views/SignIn";
 
-const app = document.querySelector<HTMLDivElement>('#app')!;
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
 // Render Layout
-app.innerHTML = Layout();
+app.innerHTML = Layout(SignIn());
 
 // Navbar eventListener
 setupNavbarListeners();
 
 // Back eventListener
-const backBtn = document.getElementById('back-button');
-backBtn?.addEventListener('click', handleBackClick);
+const backBtn = document.getElementById("back-button");
+backBtn?.addEventListener("click", handleBackClick);
