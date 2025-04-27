1d510595655c833aca984e525544237e32d680b3
Lara <lara.salman01@hotmail.com>
2025-04-27 04:09:57 +0200

    fetch delete button

--- src/components/DeleteButton.ts
+++ src/components/DeleteButton.ts
@@ -1,7 +1,9 @@
-export function DeleteButton(): string {
+export function DeleteButton(projectId: string): string {
   return `
-      <button class="delete-btn text-shade-800 text-[1.125rem] hover:text-red active:text-red transition-colors duration-300 cursor-pointer">
-        <span class="iconify" data-icon="weui:delete-outlined"></span>
-      </button>
-    `;
+    <button 
+      class="delete-btn text-shade-800 text-[1.125rem] hover:text-red active:text-red transition-colors duration-300 cursor-pointer" 
+      data-id="${projectId}">
+      <span class="iconify" data-icon="weui:delete-outlined"></span>
+    </button>
+  `;
 }
